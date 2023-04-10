53d5af739aafeda9beaa23362c3e8164faf33117
kiny123 <89192734912nik@gmail.com>
2023-04-11 00:19:44 +0300

    init

---
+++ Timofey's Challenge/Timofey's Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timofey's Challenge
+//
+//  Created by nikita on 10.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
